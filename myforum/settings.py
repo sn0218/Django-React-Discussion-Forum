d46422ebf744820714a1e1998240d99a993ec6c0
sn0218 <sn02318@gmail.com>
2022-08-25 01:57:49 +0800

    add whitenoise in settings.py

--- myforum/settings.py
+++ myforum/settings.py
@@ -93,6 +93,7 @@ SIMPLE_JWT = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
