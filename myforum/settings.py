63549c434081d8e8e9823a1737da822d8387b8da
sn0218 <sn02318@gmail.com>
2022-08-25 17:18:26 +0800

    fix static

--- myforum/settings.py
+++ myforum/settings.py
@@ -177,10 +177,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 # Important for Heroku
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATIC_URL = '/react/frontend/build/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'react/frontend/build/static'), )
 
 
