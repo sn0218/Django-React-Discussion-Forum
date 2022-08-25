a1c7b0606d8b4305895700c5a0f7cd8bea8e1151
sn0218 <sn02318@gmail.com>
2022-08-25 21:15:43 +0800

    fix static

--- myforum/settings.py
+++ myforum/settings.py
@@ -183,7 +183,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'frontend/build/static'),)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
