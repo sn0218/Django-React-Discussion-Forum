71b4b9b32d3d6b91d6d7f8e0e28f5498a4eaaf8b
sn0218 <sn02318@gmail.com>
2022-08-25 17:43:46 +0800

    fix static

--- myforum/settings.py
+++ myforum/settings.py
@@ -182,7 +182,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'frontend/build/static'), )
 
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
