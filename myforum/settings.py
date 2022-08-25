721cd801bb95029883bfb563dd6e07a30fd18098
sn0218 <sn02318@gmail.com>
2022-08-25 16:37:41 +0800

    fix dirs

--- myforum/settings.py
+++ myforum/settings.py
@@ -174,10 +174,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Important for Heroku
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, 'react/frontend/build/static') 
+   BASE_DIR /  'react/frontend/build/static'
 ]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
