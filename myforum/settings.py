c28ee6a1a97333a27fd1fbc1ff9c8594e841bea0
sn0218 <sn02318@gmail.com>
2022-08-25 18:30:54 +0800

    fix urls

--- myforum/settings.py
+++ myforum/settings.py
@@ -15,7 +15,7 @@ from datetime import timedelta
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -135,7 +135,7 @@ WSGI_APPLICATION = 'myforum.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
     }
 }
 
@@ -178,7 +178,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 
-STATIC_ROOT = BASE_DIR / 'app/static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'app/static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'frontend/build/static'), )
 
