ea1b8c6be6fddb238f60b47932bb59b66a7daa3e
sn0218 <sn02318@gmail.com>
2022-08-25 17:28:31 +0800

    fix static

--- myforum/settings.py
+++ myforum/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -176,12 +177,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-# Important for Heroku
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'react/frontend/build/static'), )
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'react\frontend\build\static'), )
 
 
 # Default primary key field type
