7c58c5d08790d4eeffad9ce58f1fc14101d34c70
sn0218 <sn02318@gmail.com>
2022-08-25 20:37:15 +0800

    fix static

--- myforum/settings.py
+++ myforum/settings.py
@@ -179,7 +179,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'frontend/build/static'),
 os.path.join(BASE_DIR, 'static'), )
