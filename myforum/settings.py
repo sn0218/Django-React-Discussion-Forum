a003a59f90d76162fb42b0230fc53c9d5e3df470
sn0218 <sn02318@gmail.com>
2022-08-25 17:35:47 +0800

    fix static

--- myforum/settings.py
+++ myforum/settings.py
@@ -112,7 +112,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'react/frontend/build')
+            os.path.join(BASE_DIR, 'frontend/build')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -180,7 +180,7 @@ USE_TZ = True
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'react\frontend\build\static'), )
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'frontend/build/static'), )
 
 
 # Default primary key field type
