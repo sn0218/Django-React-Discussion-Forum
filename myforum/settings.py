69841cc504faa3be8205c9046fb62ffce2e384f4
sn0218 <sn02318@gmail.com>
2022-08-25 20:40:30 +0800

    fix static

--- myforum/settings.py
+++ myforum/settings.py
@@ -179,9 +179,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'frontend/build/static'),
-os.path.join(BASE_DIR, 'static'), )
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'frontend/build/static'),)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
